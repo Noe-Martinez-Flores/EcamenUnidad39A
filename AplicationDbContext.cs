a944a115ec4e3968c8bde512d6e28efd4d8521b1
Noe-Mtz <noe.mtz1630@gmail.com>
2023-08-19 00:03:57 -0600

    [ADD] Medicamentos consumed

--- AplicationDbContext.cs
+++ AplicationDbContext.cs
@@ -1,12 +1,33 @@
+using System;
+using ExamenUnidad3.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ExamenUnidad3
 {
-    class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
         }
+
+        public DbSet<Medicamento> ? Medicamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicamento>().HasData(
+                new Medicamento()
+                {
+                    Id = 1,
+                     Descripcion = "HOLA",
+                     DosisRecomendada = 1,
+                      FormaAdministracion = "ORAL",
+                      Indicaciones = "NA",
+                      Nombre = "PARACETAMIOL"
+                }
+            );
+        }
+
     }
 }
